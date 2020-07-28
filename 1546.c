2af436e7e30aef0c6becca5ee1a2a27fe1a861e5
hyomin14 <hyominstar@naver.com>
2020-07-28 15:38:16 +0900

    C

--- 1546.c
+++ 1546.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-   int num, max = 0;
-   int score[1000];
-   double sum = 0;
-   scanf("%d", &num);
-   for(int i = 0; i < num; i++){
-      scanf("%d", &score[i]);
-      if(max < score[i])      max = score[i];
-   }
-   for(int i = 0; i < num; i++)
-      sum += (double)score[i] / max * 100;
-   printf("%f", sum / num);
-   return 0;
+	int num, max = 0;
+	int score[1000];
+	double sum = 0;
+	scanf("%d", &num);
+	for(int i = 0; i < num; i++){
+		scanf("%d", &score[i]);
+		if(max < score[i])		max = score[i];
+	}
+	for(int i = 0; i < num; i++)
+		sum += (double)score[i] / max * 100;
+	printf("%f", sum / num);
+	return 0;
 }
--- 4344.c
+++ 4344.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main(void)
 {
-   int c;
-   scanf("%d", &c);
-   for(int i = 0; i < c; i++){
-      int n, count = 0, sum = 0;
-      double ave;
-      int score[1000];
-      scanf("%d", &n);
-      for(int j = 0; j < n; j++)   {   
-         scanf("%d", &score[j]);
-         sum += score[j];
-      }
-      ave = sum / n;   
-      for(int j = 0; j < n; j++)   {
-      if (score[j] > ave)      count++;}   
-      printf("%.3f%%\n", (double)count / n * 100);
-   }
-   return 0;
+	int c;
+	scanf("%d", &c);
+	for(int i = 0; i < c; i++){
+		int n, count = 0, sum = 0;
+		double ave;
+		int score[1000];
+		scanf("%d", &n);
+		for(int j = 0; j < n; j++)	{	
+			scanf("%d", &score[j]);
+			sum += score[j];
+		}
+		ave = sum / n;	
+		for(int j = 0; j < n; j++)	{
+		if (score[j] > ave)		count++;}	
+		printf("%.3f%%\n", (double)count / n * 100);
+	}
+	return 0;
 }
